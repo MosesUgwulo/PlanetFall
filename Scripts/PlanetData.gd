2380ba2140ef6bd71e9c938b9c989a0915aaf393
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2024-11-14 13:00:01 +0000

    Added multiple layers of noise

--- Scripts/PlanetData.gd
+++ Scripts/PlanetData.gd
@@ -11,56 +11,33 @@ class_name PlanetData
 func point_on_planet(point_on_sphere : Vector3) -> Vector3:
 	if noise_layers.is_empty():
 		return point_on_sphere * radius
-	
-	var base_elevation := 0.0
+
+
 	var first_layer_raw := 0.0
+	var total_elevation = 0.0
 
-	if noise_layers.size() > 0 and noise_layers[0] != null and noise_layers[0].noise != null:
-		
-		var sample_point = point_on_sphere * noise_layers[0].scale_factor
-		var first_noise = noise_layers[0].noise.get_noise_3dv(sample_point)
-
-		first_layer_raw = (first_noise + 1.0) * 0.5
-
-		base_elevation = first_layer_raw * noise_layers[0].amplitude
-		base_elevation = max(0.0, base_elevation - noise_layers[0].min_height)
+	if noise_layers[0] != null:
+		first_layer_raw = noise_layers[0].evaluate(point_on_sphere)
+		total_elevation = first_layer_raw
 	
-	var total_elevation := base_elevation
-
 	for i in range(1, noise_layers.size()):
-
 		var layer := noise_layers[i]
-		if layer == null or layer.noise == null:
+		if layer == null:
 			continue
 		
-		var mask := 1.0
-
-		if layer.use_first_layer_as_mask:
-			mask = first_layer_raw
-			mask = pow(mask, 2.0)
-
-			if base_elevation <= 0:
-				continue
-		
-		var sample_point = point_on_sphere * layer.scale_factor
-		var noise_val = layer.noise.get_noise_3dv(sample_point)
-
-		noise_val = (noise_val + 1.0) * 0.5
-		noise_val = noise_val * layer.amplitude * mask
-		noise_val = max(0.0, noise_val - layer.min_height)
-
-		total_elevation += noise_val
-
-	return point_on_sphere * radius * (total_elevation + 1.0)
+		var noise_value = layer.evaluate(point_on_sphere, first_layer_raw)
+		total_elevation += noise_value
+	
+	return point_on_sphere * radius * (1.0 + total_elevation)
 
 
 func set_noise_layers(value):
 	noise_layers = value
 	emit_signal("changed")
 
-	for noise_layer in noise_layers:
-		if noise_layer != null and not noise_layer.is_connected("changed", _on_noise_changed):
-			noise_layer.connect("changed", _on_noise_changed)
+	for layer in noise_layers:
+		if layer != null and not layer.is_connected("changed", _on_noise_changed):
+			layer.connect("changed", _on_noise_changed)
 
 
 func set_radius(value):
--- Scripts/PlanetNoise.gd
+++ Scripts/PlanetNoise.gd
@@ -3,39 +3,89 @@ extends Resource
 class_name PlanetNoise
 
 @export var noise : FastNoiseLite = null : set = set_noise
-@export var amplitude : float = 1.0 : set = set_amplitude
+@export var strength : float = 1.0 : set = set_strength
+@export_range(1, 8, 1) var num_layers : int = 1 : set = set_num_layers
+@export var base_roughness : float = 1.0 : set = set_base_roughness
+@export var roughness : float = 2.0 : set = set_roughness
+@export var persistence : float = 0.5 : set = set_persistence
+@export var center : Vector3 = Vector3.ZERO : set = set_center
 @export var min_height : float = 0.0 : set = set_min_height
 @export var use_first_layer_as_mask : bool = false : set = set_use_mask
-@export var scale_factor : float = 1.0 : set = set_scale_factor
+
+
+
+func evaluate(point : Vector3, mask_value: float = 1.0) -> float:
+	if noise == null:
+		return 0.0
+	
+	var noise_value: float = 0.0
+	var frequency: float = base_roughness
+	var amplitude: float = 1.0
+
+	for i in num_layers:
+		var noise_point = point * frequency + center
+		var v = noise.get_noise_3dv(noise_point)
+
+		noise_value += (v + 1.0) * 0.5 * amplitude
+
+		frequency *= roughness
+		amplitude *= persistence
+	
+	noise_value = maxf(0.0, noise_value - min_height)
+
+	if use_first_layer_as_mask:
+		noise_value *= pow(mask_value, 2.0)
+
+	return noise_value * strength
 
 
 func set_noise(value):
-    noise = value
-    emit_signal("changed")
-    
-    if noise != null and not noise.is_connected("changed", _on_noise_changed):
-        noise.connect("changed", _on_noise_changed)
+	noise = value
+	emit_signal("changed")
+	
+	if noise != null and not noise.is_connected("changed", _on_noise_changed):
+		noise.connect("changed", _on_noise_changed)
 
 
-func set_amplitude(value):
-    amplitude = value
-    emit_signal("changed")
+func set_strength(value):
+	strength = value
+	emit_signal("changed")
+
+
+func set_num_layers(value):
+	num_layers = value
+	emit_signal("changed")
+
+
+func set_base_roughness(value):
+	base_roughness = value
+	emit_signal("changed")
+
+
+func set_roughness(value):
+	roughness = value
+	emit_signal("changed")
+
+
+func set_persistence(value):
+	persistence = value
+	emit_signal("changed")
+
+
+func set_center(value):
+	center = value
+	emit_signal("changed")
 
 
 func set_min_height(value):
-    min_height = value
-    emit_signal("changed")
+	min_height = value
+	emit_signal("changed")
 
 
 func set_use_mask(value):
-    use_first_layer_as_mask = value
-    emit_signal("changed")
-
-
-func set_scale_factor(value):
-    scale_factor = value
-    emit_signal("changed")
+	use_first_layer_as_mask = value
+	emit_signal("changed")
 
 
 func _on_noise_changed():
-    emit_signal("changed")
+	emit_signal("changed")
