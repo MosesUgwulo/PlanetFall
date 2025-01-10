00d2b3831db339060ccc1e523efb2476024de0c5
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2025-01-10 22:55:10 +0000

    Added Biome settings button and dropdown

--- Scripts/UIManager.gd
+++ Scripts/UIManager.gd
@@ -5,17 +5,23 @@ extends CanvasLayer
 # ============================= NOISE SETTINGS =============================
 @onready var noise_settings_btn : Button = get_node("RootControl/MarginContainer/Settings/Noise Settings Button/Noise Settings")
 @onready var noise_settings_dropdown : HBoxContainer = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown")
-@onready var seedLineEdit : LineEdit = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown/Noise Settings Container/Seed Setting/Seed Value")
-@onready var frequencyLineEdit : LineEdit = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown/Noise Settings Container/Frequency Setting/Frequency LineEdit")
-@onready var frequencySlider : HSlider = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown/Noise Settings Container/Frequency Setting/Frequency Value")
-@onready var weightedStrengthLineEdit : LineEdit = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown/Noise Settings Container/Weighted Strength Setting/Weighted Strength LineEdit")
-@onready var weightedStrengthSlider : HSlider = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown/Noise Settings Container/Weighted Strength Setting/Weighted Strength Value")
+@onready var seed_line_edit : LineEdit = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown/Noise Settings Container/Seed Setting/Seed Value")
+@onready var frequency_line_edit : LineEdit = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown/Noise Settings Container/Frequency Setting/Frequency LineEdit")
+@onready var frequency_slider : HSlider = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown/Noise Settings Container/Frequency Setting/Frequency Value")
+@onready var weighted_strength_line_edit : LineEdit = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown/Noise Settings Container/Weighted Strength Setting/Weighted Strength LineEdit")
+@onready var weighted_strength_slider : HSlider = get_node("RootControl/MarginContainer/Settings/Noise Settings Dropdown/Noise Settings Container/Weighted Strength Setting/Weighted Strength Value")
+
+
+# ============================= BIOME SETTINGS =============================
+@onready var biome_settings_btn : Button = get_node("RootControl/MarginContainer/Settings/Biome Settings Button/Biome Settings")
+@onready var biome_settings_dropdown : HBoxContainer = get_node("RootControl/MarginContainer/Settings/Biome Settings Dropdown")
 
 @onready var generateBtn : Button = get_node("RootControl/MarginContainer/Settings/GenerateBtnVBox/Generate Button")
 
 # TODO: ADD TOOL TIPS TO EVERYTHING
 
 # Editible values
+# ============================= NOISE SETTINGS =============================
 var seedValue : int = 0
 var frequency : float = 0.01
 var scale_factor : float = 1.0
@@ -33,16 +39,16 @@ var subdivisions : int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 
-	frequencySlider.min_value = 0.0001
-	frequencySlider.max_value = 1.0
-	frequencySlider.step = 0.0001
-	frequencySlider.value = 0.01
-	frequencyLineEdit.text = str(frequencySlider.value)
+	frequency_slider.min_value = 0.0001
+	frequency_slider.max_value = 1.0
+	frequency_slider.step = 0.0001
+	frequency_slider.value = 0.01
+	frequency_line_edit.text = str(frequency_slider.value)
 
 	if planet and planet.get_child(0):
 
-		# planet.planet_data = PlanetData.new()
-		seedLineEdit.text = str(seedValue)
+		planet.planet_data = PlanetData.new()
+		seed_line_edit.text = str(seedValue)
 
 
 		var mesh = planet.get_child(0) as PlanetMesh
@@ -50,10 +56,12 @@ func _ready():
 			mesh.generate_status_changed.connect(_on_generate_status_changed)
 
 	noise_settings_dropdown.visible = false
+	biome_settings_dropdown.visible = false
 	
 
 
 func _generate_planet():
+	# ============================= NOISE SETTINGS =============================
 	planet.planet_data.planet_noise.noise.seed = seedValue
 	planet.planet_data.planet_noise.noise.frequency = frequency
 	planet.planet_data.planet_noise.scale_factor = scale_factor
@@ -63,13 +71,14 @@ func _generate_planet():
 	planet.planet_data.planet_noise.noise.fractal_gain = fractal_gain
 	planet.planet_data.planet_noise.noise.fractal_weighted_strength = fractal_weighted_strength
 
+
 	planet.planet_data.radius = radius
 	planet.planet_data.subdivisions = subdivisions
 	
 	planet._on_planet_changed()
 
 
-
+# ============================= NOISE SETTINGS =============================
 func _on_noise_settings_button_toggled():
 	noise_settings_dropdown.visible = !noise_settings_dropdown.visible
 	noise_settings_btn.text = "▼ Noise Settings" if noise_settings_dropdown.visible else "▶ Noise Settings"
@@ -101,20 +110,20 @@ func _on_seed_value_text_changed(new_text):
 
 
 func _on_frequency_value_changed(value : float):
-	frequencyLineEdit.text = str(value)
+	frequency_line_edit.text = str(value)
 	frequency = value
 
 
 
 func _on_frequency_line_edit_text_changed(new_text : String):
-	frequencySlider.value = float(new_text)
-	frequency = frequencySlider.value
+	frequency_slider.value = float(new_text)
+	frequency = frequency_slider.value
 
 
 
 func _on_frequency_line_edit_focus_exited():
-	frequencySlider.value = float(frequencyLineEdit.text)
-	frequency = frequencySlider.value
+	frequency_slider.value = float(frequency_line_edit.text)
+	frequency = frequency_slider.value
 
 
 
@@ -152,24 +161,27 @@ func _on_gain_value_changed(value:float):
 
 
 func _on_weighted_strength_line_edit_text_submitted(new_text : String):
-	weightedStrengthSlider.value = float(new_text)
-	fractal_weighted_strength = weightedStrengthSlider.value
+	weighted_strength_slider.value = float(new_text)
+	fractal_weighted_strength = weighted_strength_slider.value
 
 
 
 func _on_weighted_strength_line_edit_focus_exited():
-	weightedStrengthSlider.value = float(weightedStrengthLineEdit.text)
-	fractal_weighted_strength = weightedStrengthSlider.value
+	weighted_strength_slider.value = float(weighted_strength_line_edit.text)
+	fractal_weighted_strength = weighted_strength_slider.value
 
 
 
 func _on_weighted_strength_value_changed(value : float):
-	weightedStrengthLineEdit.text = str(value)
+	weighted_strength_line_edit.text = str(value)
 	fractal_weighted_strength = value
 
 
 
-
+# ============================= BIOME SETTINGS =============================
+func _on_biome_settings_pressed():
+	biome_settings_dropdown.visible = !biome_settings_dropdown.visible
+	biome_settings_btn.text = "▼ Biome Settings" if biome_settings_dropdown.visible else "▶ Biome Settings"
 
 
 
@@ -192,6 +204,8 @@ func _on_generate_status_changed(is_generating : bool):
 	if generateBtn:
 		generateBtn.disabled = is_generating
 		generateBtn.text = "Generating..." if is_generating else "Generate"
+
+
 
 
 
